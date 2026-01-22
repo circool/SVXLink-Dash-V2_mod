9ae8453554ad48f3629d7235b135f7ced2d99731
Vladimir <circool@mail.ru>
2026-01-22 14:40:56 +0300

    Update getLineTime.0.1.2.php

--- html/include/fn/exct/getLineTime.0.1.2.php
+++ html/include/fn/exct/getLineTime.0.1.2.php
@@ -8,7 +8,7 @@
 function getLineTime(string $line): int
 {
 	$timeZone = isset($_SESSION['TIMEZONE']) ? $_SESSION['TIMEZONE'] : "UTC";
-
+	
 	// Ищем позицию ": " в строке
 	$pos = strpos($line, ': ');
 
